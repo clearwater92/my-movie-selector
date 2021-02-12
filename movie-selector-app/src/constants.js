352d8acdbda5b9b332e2ac31f7a81271743e59b2
clearwater <clearwater92@naver.com>
2021-02-12 16:19:56 +0900

    json-server url change

--- movie-selector-app/src/constants.js
+++ movie-selector-app/src/constants.js
@@ -6,4 +6,4 @@ export const SEARCH_API = `${BASE_TMDB}search/movie?&api_key=${API_KEY}&query=`;
 export const FEATURED_API = `${BASE_TMDB}discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`;
 export const IMAGE_NOT_PREPARED =
   'https://images.unsplash.com/photo-1509281373149-e957c6296406?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=369&q=80';
-export const JSON_SERVER_URL = 'http://localhost:3001';
+export const JSON_SERVER_URL = 'https://jsonserver-clearwater.herokuapp.com';
